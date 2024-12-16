76e15d90524f8702cbe28f7c7775b95e578f71c7
glab254-EE <71868718+glab254-EE@users.noreply.github.com>
2024-12-16 13:47:31 +0500

    Fixes

--- Assets/LogDeformer.cs
+++ Assets/LogDeformer.cs
@@ -2,24 +2,25 @@ using UnityEngine;
 
 public class LogDeformer : MonoBehaviour
 {
-    [SerializeField] Transform water;
-    [SerializeField] float wavepower;
-    float rotxdif = 1f;
-    float rotydif = 1f;
+    [SerializeField] private Transform Water;
+    [SerializeField] private float GeneralWaterPower = 0.5f;
+    [SerializeField] private float RotationXTimeSpeed = 1.3f;
+    [SerializeField] private float RotationYTimeSpeed = 1.3f;
+    [SerializeField] private float UpDownTimeSpeed = 1.5f;
+    [SerializeField] private float UpDownAmmountMultiplier = 0.25f;
+    [SerializeField] private float GeneralRotationPower = 5f;
     Vector3 basepos;
     void Start()
     {
-        basepos = new Vector3(transform.position.x,water.position.y,transform.position.z);
-        rotxdif = Random.Range(0.8f, 1.4f);
-        rotydif = Random.Range(0.8f, 1.4f);
+        basepos = new Vector3(transform.position.x,Water.position.y,transform.position.z);
     }
     float tick = 0;
-    private void Update()
+    void Update()
     {
         tick += Time.deltaTime;
-        float rotx = Mathf.Sin(tick*rotxdif)*5* wavepower;
-        float roty = Mathf.Cos(tick*rotydif)*5* wavepower;
-        float moveTD = Mathf.Sin(tick * 1.5f)* wavepower*0.25f;
+        float rotx = Mathf.Sin(tick* RotationXTimeSpeed) * GeneralRotationPower * GeneralWaterPower;
+        float roty = Mathf.Cos(tick* RotationYTimeSpeed) * GeneralRotationPower * GeneralWaterPower;
+        float moveTD = Mathf.Sin(tick * UpDownTimeSpeed) * GeneralWaterPower* UpDownAmmountMultiplier;
         transform.rotation = Quaternion.Euler(rotx, 0, roty);
         transform.position = basepos + transform.up * moveTD;
     }
